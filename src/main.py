48c344d14e53d45ceae3d560cd0b319c0d32bc41
Pierre Chojka <pchojka@gmail.com>
2021-09-29 09:13:01 +0200

    Add skeleton

---
+++ src/main.py
@@ -0,0 +1,21 @@
+import os
+from discord.ext import commands
+
+bot = commands.Bot(
+    command_prefix="!",  # Change to desired prefix
+    case_insensitive=True  # Commands aren't case-sensitive
+)
+
+bot.author_id = 0000000  # Change to your discord id!!!
+
+@bot.event
+async def on_ready():  # When the bot is ready
+    print("I'm in")
+    print(bot.user)  # Prints the bot's username and identifier
+
+@bot.command()
+async def pong(ctx):
+    await ctx.send('pong')
+
+token = "<MY_SUPER_TOKEN>"
+bot.run(token)  # Starts the bot
